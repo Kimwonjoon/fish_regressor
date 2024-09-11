c5fe5b8b598e0a2390f596d52c46eecdeeeb9f74
Kimwonjoon <kimpass189@gmail.com>
2024-09-11 09:02:33 +0900

    model path

--- src/fr/api/weightpd.py
+++ src/fr/api/weightpd.py
@@ -1,12 +1,18 @@
+import sklearn
+import pickle
+import os
+def get_model_path():
+    my_path = __file__
+    dir_name = os.path.dirname(my_path)
+    model_path = os.path.join(dir_name, "lr_model.pkl")
+    return model_path
+
 def lr_api(length):
 #    import numpy as np
 #    from sklearn.model_selection import train_test_split
 #    from sklearn.linear_model import LinearRegression
-    import sklearn #for predict
-    import pickle
-    import os
-
-    with open("lr_model.pkl", "rb") as f:
+    path = get_model_path()
+    with open(path, "rb") as f:
         fish_lr = pickle.load(f)
 
     lr_w = fish_lr.predict([[length ** 2, length]])
